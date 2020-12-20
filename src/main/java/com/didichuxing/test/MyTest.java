86f2e03a6e10a27fee7e639578dce2ea07457a0e
liuweisheng <liuweisheng8149199@163.com>
2020-12-20 21:16:14 +0800

    提交测试分支

---
+++ src/main/java/com/didichuxing/test/MyTest.java
@@ -0,0 +1,10 @@
+package com.didichuxing.test;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("这是我的测试分支");
+    }
+}
